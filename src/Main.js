447a8e135b6d476862f73ef4021258bb6c38ac6d
Himmet Batuhan Onge <himmet.b.onge@gmail.com>
2023-06-19 20:20:30 +0300

    Readded markdown preview

--- src/Main.js
+++ src/Main.js
@@ -1,3 +1,5 @@
+import { ReactMarkdown } from "react-markdown/lib/react-markdown"
+
 function Main({activeNote, onUpdateNote}){
   const onEditField = (key, value) =>{
     onUpdateNote({
@@ -15,7 +17,10 @@ function Main({activeNote, onUpdateNote}){
       <input type="text" id="title" value={activeNote.title} onChange={(e) => onEditField('title', e.target.value)} autoFocus />
       <textarea id="body" placeholder="Write your note here..." value={activeNote.body} onChange={(e) => onEditField('body', e.target.value)}  />
     </div>
-    
+    <div className="app-main-note-preview">
+      <h1 className="preview-title">{activeNote.title}</h1>
+      <ReactMarkdown className="markdown-preview">{activeNote.body}</ReactMarkdown>
+    </div>
   </div>
 )}
 
